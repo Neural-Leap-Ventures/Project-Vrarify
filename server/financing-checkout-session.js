5dd27480c7785c4fa7e8890f1aa5d69deda09538
William-Boomer-Baker <61856702+William-Boomer-Baker@users.noreply.github.com>
2024-02-08 22:11:13 -0800

    Update financing-checkout-session.js

--- server/financing-checkout-session.js
+++ server/financing-checkout-session.js
@@ -15,8 +15,6 @@ exports.handler = async (event, context) => {
         expand: ['data.product'],
     });
 
-    console.log("Prices: " + prices)
-
     try {
         if (trial_days == 0) {
             const session = await stripe.checkout.sessions.create({
